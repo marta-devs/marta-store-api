e8b37f54288f5d1898159dcdf3f293bf287ec3e2
Walany Costa <walanybnegro@gmail.com>
2024-12-14 21:55:18 +0100

    test: ensure RemoveProductRepository throws error if FindProductByIdAndStatusRepository returns null

--- src/application/module-product/product/remove-product/remove-product-server.spec.ts
+++ src/application/module-product/product/remove-product/remove-product-server.spec.ts
@@ -1,21 +1,48 @@
-import { RemoveProductRepository } from 'infra/protocols/remove-product-repository.js';
+import { RemoveProductRepository } from 'infra/protocols/remove-product-repository';
 import { expect, test, vi, describe } from 'vitest';
-import { ProductStatus, RemoveProductService } from './remove-product-service.js';
+import { RemoveProductService } from './remove-product-service';
+import { Product, ProductStatus } from 'domain/product';
+import { FindProductByIdAndStatusRepository } from 'infra/protocols/find-product-by-id-and-status-remove-repository';
 
 interface SutType {
-  sut: RemoveProductService
-  removeProductRepositoryStub: RemoveProductRepository
+	sut: RemoveProductService;
+	removeProductRepositoryStub: RemoveProductRepository;
+	findProductByIdAndStatusRepositoryStub: FindProductByIdAndStatusRepository;
 }
 
-const makeSut = ():SutType => {
-  const removeProductRepositoryStub = makeRemoveProductRepositoryStub();
-  const sut = new RemoveProductService(removeProductRepositoryStub);
+const makeSut = (): SutType => {
+	const removeProductRepositoryStub = makeRemoveProductRepositoryStub();
+	const findProductByIdAndStatusRepositoryStub =
+		makeFindProductByIdAndStatusRepositoryStub();
+	const sut = new RemoveProductService(
+		removeProductRepositoryStub,
+		findProductByIdAndStatusRepositoryStub,
+	);
 
-  return {
-    sut,
-    removeProductRepositoryStub
-  }
-}
+	return {
+		sut,
+		removeProductRepositoryStub,
+		findProductByIdAndStatusRepositoryStub,
+	};
+};
+
+const makeFindProductByIdAndStatusRepositoryStub =
+	(): FindProductByIdAndStatusRepository => {
+		class FindProductByIdAndStatusRepositoryStub
+			implements FindProductByIdAndStatusRepository
+		{
+			async loadByIdAndStatusRemoved(): Promise<Product> {
+				return Promise.resolve(
+					new Product({
+						id: 'any_id',
+						name: 'any_name',
+						isExpiration: false,
+					}),
+				);
+			}
+		}
+		return new FindProductByIdAndStatusRepositoryStub();
+	};
 
 const makeRemoveProductRepositoryStub = (): RemoveProductRepository => {
 	class RemoveProductRepositoryStub implements RemoveProductRepository {
@@ -30,17 +57,28 @@ const makeRemoveProductRepositoryStub = (): RemoveProductRepository => {
 describe('RemoveProductService', () => {
 	test('should call RemoveProductRepository with correct params', async () => {
 		const productId = 'any_id';
-    const {sut, removeProductRepositoryStub} = makeSut()
+		const { sut, removeProductRepositoryStub } = makeSut();
 		const removeSpy = vi.spyOn(removeProductRepositoryStub, 'remove');
 		await sut.execute(productId);
 		expect(removeSpy).toHaveBeenCalledWith(productId, ProductStatus.REMOVED);
 	});
 
-  test('should throw if RemoveProductRepository throws', async () => {
-		const productId = 'any_id';
-    const {sut, removeProductRepositoryStub} = makeSut()
-		vi.spyOn(removeProductRepositoryStub, 'remove').mockRejectedValueOnce(new Error())
-		const response = sut.execute(productId);
-		expect(response).rejects.toThrowError();
+	test('should throw if RemoveProductRepository throws', async () => {
+		const { sut, removeProductRepositoryStub } = makeSut();
+		vi.spyOn(removeProductRepositoryStub, 'remove').mockRejectedValueOnce(
+			new Error(),
+		);
+		const response = sut.execute('any_id');
+		await expect(response).rejects.toThrowError();
+	});
+
+	test('should throw if FindProductByIdAndStatusRepository return null', async () => {
+		const { sut, findProductByIdAndStatusRepositoryStub } = makeSut();
+		vi.spyOn(
+			findProductByIdAndStatusRepositoryStub,
+			'loadByIdAndStatusRemoved',
+		).mockResolvedValueOnce(null);
+		const response = sut.execute('any_id');
+		await expect(response).rejects.toThrow('Product is not exists!');
 	});
 });
